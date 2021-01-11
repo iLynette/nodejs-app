476775050393951bd1e6b2d44b828251a48656ba
iLynette <lynetteacholah@icloud.com>
2021-01-11 18:58:18 +0300

    oauth callbacks and access

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
 const express = require('express')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-const keys = require('./config/keys')
-const app = express()
+require('./services/passport')
 
-passport.use(new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback'
-}, 
-accessToken => {
-  console.log(accessToken)
-}
-))
-//route handler
-app.get('/auth/google',
-passport.authenticate('google', {
-  scope: ['profile', 'email']
-}))
+const app = express()
 
 //route handler
 // app.get('/', (req, res) => {
 //   res.send({ hi: 'Lynette' })
 // })
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
