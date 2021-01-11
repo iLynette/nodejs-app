89625cf0eb6f63c2c35c8d7fac226d61e15b5d4f
iLynette <lynetteacholah@icloud.com>
2021-01-11 11:45:12 +0300

    passport setup

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-strategy').strategy
+
 const app = express()
 
+passport.use(new GoogleStrategy())
+
 //route handler
-app.get('/', (req, res) => {
-  res.send({ hi: 'Lynette' })
-})
+// app.get('/', (req, res) => {
+//   res.send({ hi: 'Lynette' })
+// })
 
 const PORT = process.env.PORT || 5000
 app.listen(5000)
